bf927d89ea5038720e39994739341f8b68acfe09
Jorrit <jorrit_l@hotmail.com>
2016-09-02 20:05:38 +0200

    structured pieces and started pawn class

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -1,5 +1,6 @@
 # Abstract class that all specific pieces inherit from
 class Piece
+	
 	attr_accessor :pos
 	attr_reader :colour
 
@@ -7,4 +8,29 @@ class Piece
 		@pos = pos
 		@colour = colour
 	end
+end
+
+# Class for spawning pawns, containg info on legal moves
+class Pawn < Piece
+	
+	attr_reader :moved?
+
+	def initialize
+		@moved? = false
+	end
+end
+
+class Queen < Piece
+end
+
+class King < Piece
+end
+
+class Rook < Piece
+end
+
+class Bishop < Piece
+end
+
+class Knight < Piece
 end
