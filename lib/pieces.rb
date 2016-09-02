a95ac0cb4e81eed7922ba27f50c88c77b2260862
Jorrit <jorrit_l@hotmail.com>
2016-09-02 20:00:40 +0200

    refactoring abstract piece class

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -1,4 +1,10 @@
 # Abstract class that all specific pieces inherit from
 class Piece
-	attr_accessor :pos, :colour
+	attr_accessor :pos
+	attr_reader :colour
+
+	def initialize(pos, colour)
+		@pos = pos
+		@colour = colour
+	end
 end
